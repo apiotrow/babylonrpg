8a5eb4306ab51fab23fd8dc5d9e02d21472ca867
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2017-08-08 01:47:43 -0700

    rendering random map with perlin noise

--- js/test.js
+++ js/test.js
@@ -1,5 +1,5 @@
 var BABYLON = require('babylonjs')
-
+let perlin = require('perlin-noise')
 class test{
 	constructor(engine, canvas, scene, tasks){
 		// let model = task.loadedMeshes[0]
@@ -14,8 +14,10 @@ class test{
 			D: 68
 		}
 
-		scene.clearColor = new BABYLON.Color3(0, 1, 0)
-		var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 20, -10), scene)
+		scene.clearColor = new BABYLON.Color3(0, 0, 0)
+		// BABYLON.SceneOptimizer.OptimizeAsync(scene, BABYLON.SceneOptimizerOptions.LowDegradationAllowed())
+		var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(100, 200, -100), scene)
+		camera.target = new BABYLON.Vector3(150, 150, 150)
 		// camera.inputs.add(new BABYLON.FreeCameraKeyboardMoveInput())
 		
 		// camera.mode = BABYLON.Camera.ORTHOGRAPHIC_CAMERA;
@@ -23,6 +25,19 @@ class test{
 		// camera.orthoBottom = -50;
 		// camera.orthoLeft = -50;
 		// camera.orthoRight = 50
+		
+
+		// var skybox = BABYLON.Mesh.CreateBox("skyBox", 1000.0, scene);
+		// var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene)
+		// skyboxMaterial.backFaceCulling = false;
+		// // skyboxMaterial.disableLighting = true;
+		// // skybox.infiniteDistance = true;
+		// // skyboxMaterial.disableLighting = true;
+		// skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("assets/skyboxes/TropicalSunnyDay/TropicalSunnyDay", scene);
+		// skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+		// skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+		// skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+		// skybox.material = skyboxMaterial;
 		
 		this.keyState = {}
 		var onKeyDown = (evt)=> {
@@ -50,8 +65,8 @@ class test{
 	    camera.attachControl(canvas, false)
 
 	    // var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene)
-	    var light = new BABYLON.DirectionalLight("light1", new BABYLON.Vector3(.8, -1, .5), scene)
-	    var light2 = new BABYLON.DirectionalLight("light2", new BABYLON.Vector3(-.8, -1, -.5), scene)
+	    var light = new BABYLON.DirectionalLight("light1", new BABYLON.Vector3(.2, -1, .5), scene)
+	    var light2 = new BABYLON.DirectionalLight("light2", new BABYLON.Vector3(-.8, -1, -.2), scene)
 	    light.intensity = 2
 	    light2.intensity = 1
 
@@ -78,6 +93,76 @@ class test{
 		// 	scene.beginAnimation(this.tasks[0].loadedMeshes[i], 0, 10, 1, 1.0)
 		// }
 		// console.log(scene.beginAnimation(this.tasks[0].loadedMeshes[0], 0, 10, 1, 1.0))
+		
+		var cell = new BABYLON.CellMaterial("cell", scene)
+			
+		cell.diffuseTexture = new BABYLON.Texture("./assets/3dd.jpg", scene)
+		cell.computeHighLevel = true
+		cell.diffuseTexture.uScale = cell.diffuseTexture.vScale = 3
+
+		console.log(tasks)
+
+		let meshes = {}
+		for(let i = 0; i < tasks.length; i++){
+			let meshName = tasks[i].name
+			let mesh = tasks[i].loadedMeshes[0]
+
+			meshes[meshName] = mesh
+		}
+
+		console.log(meshes)
+
+
+		let d = 50
+		let spacing = 15
+		for(let i in meshes){
+			let mesh = meshes[i]
+			// task.loadedMeshes[i].useVertexColors = true
+			mesh.material = cell
+
+			mesh.convertToFlatShadedMesh()
+			mesh.position.x = Math.floor(Math.random() * d) * spacing
+			mesh.position.z = Math.floor(Math.random() * d) * spacing
+			mesh.position.y = 1
+		}
+
+		
+		
+		const h = perlin.generatePerlinNoise(d, d)
+		for(let i = 0; i < d * d; i++){
+			var newInstance 
+
+			if(h[i] < 0.5)
+				newInstance = meshes.grass.createInstance("index: " + i)
+			else
+				newInstance = meshes.tree.createInstance("index: " + i)
+
+			//column
+			newInstance.position.x = (i % d) * spacing
+
+			//row
+			newInstance.position.z = (Math.floor(i / d) * spacing)
+			newInstance.position.y = 0
+		}
+
+		// for(let i = 0; i < d; i++){
+		// 	for(let j = 0; j < d; j++){
+		// 		var newInstance 
+
+		// 		if(h[i] < 0.4)
+		// 			newInstance = meshes.grass.createInstance("index: " + i + "" + j)
+		// 		else
+		// 			newInstance = meshes.tree.createInstance("index: " + i + "" + j)
+
+				
+		// 		newInstance.position.x =+ i * spacing
+		// 		newInstance.position.z =+ j * spacing
+
+		// 		// newInstance.enableEdgesRendering()
+		// 	 //   	newInstance.edgesWidth = 30.0;
+		// 		// newInstance.edgesColor = new BABYLON.Color4(0, 0, 0, 1)
+		// 	}
+		// }
 	}
 
 	update(){
--- main.js
+++ main.js
@@ -46,160 +46,63 @@ document.addEventListener('DOMContentLoaded', function () {
 		var scene = new BABYLON.Scene(engine)
 
 
-		// BABYLON.Effect.ShadersStore["customVertexShader"]=               
-		// "precision highp float;\r\n"+
-
-  //               "// Attributes\r\n"+
-  //               "attribute vec3 position;\r\n"+
-  //               "attribute vec3 normal;\r\n"+
-  //               "attribute vec2 uv;\r\n"+
-
-  //               "// Uniforms\r\n"+
-  //               "uniform mat4 world;\r\n"+
-  //               "uniform mat4 worldViewProjection;\r\n"+
-
-  //               "// Varying\r\n"+
-  //               "varying vec3 vPositionW;\r\n"+
-  //               "varying vec3 vNormalW;\r\n"+
-  //               "varying vec2 vUV;\r\n"+
-
-  //               "void main(void) {\r\n"+
-  //               "    vec4 outPosition = worldViewProjection * vec4(position, 1.0);\r\n"+
-  //               "    gl_Position = outPosition;\r\n"+
-  //               "    \r\n"+
-  //               "    vPositionW = vec3(world * vec4(position, 1.0));\r\n"+
-  //               "    vNormalW = normalize(vec3(world * vec4(normal, 0.0)));\r\n"+
-  //               "    \r\n"+
-  //               "    vUV = uv;\r\n"+
-  //               "}\r\n";
-
-  //               BABYLON.Effect.ShadersStore["customFragmentShader"]=                
-  //               "precision highp float;\r\n"+
-
-  //               "// Lights\r\n"+
-  //               "varying vec3 vPositionW;\r\n"+
-  //               "varying vec3 vNormalW;\r\n"+
-  //               "varying vec2 vUV;\r\n"+
-
-  //               "// Refs\r\n"+
-  //               "uniform vec4 _Color;"+
-		// 		"uniform float _Brightness;"+
-  //               "uniform sampler2D textureSampler;\r\n"+
-
-  //               "void main(void) {\r\n"+
-  //               "    float ToonThresholds[4];\r\n"+
-  //               "    ToonThresholds[0] = 0.95;\r\n"+
-  //               "    ToonThresholds[1] = 0.5;\r\n"+
-  //               "    ToonThresholds[2] = 0.2;\r\n"+
-  //               "    ToonThresholds[3] = 0.03;\r\n"+
-  //               "    \r\n"+
-  //               "    float ToonBrightnessLevels[5];\r\n"+
-  //               "    ToonBrightnessLevels[0] = 1.0;\r\n"+
-  //               "    ToonBrightnessLevels[1] = 0.8;\r\n"+
-  //               "    ToonBrightnessLevels[2] = 0.6;\r\n"+
-  //               "    ToonBrightnessLevels[3] = 0.35;\r\n"+
-  //               "    ToonBrightnessLevels[4] = 0.2;\r\n"+
-  //               "    \r\n"+
-  //               "    vec3 vLightPosition = vec3(0,20,10);\r\n"+
-  //               "    \r\n"+
-  //               "    // Light\r\n"+
-  //               "    vec3 lightVectorW = normalize(vLightPosition - vPositionW);\r\n"+
-  //               "    \r\n"+
-  //               "    // diffuse\r\n"+
-  //               "    float ndl = max(0., dot(vNormalW, lightVectorW));\r\n"+
-  //               "    \r\n"+
-  //               "    vec3 color = texture2D(textureSampler, vUV).rgb;\r\n"+
-  //               "    \r\n"+
-  //               "    if (ndl > ToonThresholds[0])\r\n"+
-  //               "    {\r\n"+
-  //               "        color *= ToonBrightnessLevels[0];\r\n"+
-  //               "    }\r\n"+
-  //               "    else if (ndl > ToonThresholds[1])\r\n"+
-  //               "    {\r\n"+
-  //               "        color *= ToonBrightnessLevels[1];\r\n"+
-  //               "    }\r\n"+
-  //               "    else if (ndl > ToonThresholds[2])\r\n"+
-  //               "    {\r\n"+
-  //               "        color *= ToonBrightnessLevels[2];\r\n"+
-  //               "    }\r\n"+
-  //               "    else if (ndl > ToonThresholds[3])\r\n"+
-  //               "    {\r\n"+
-  //               "        color *= ToonBrightnessLevels[3];\r\n"+
-  //               "    }\r\n"+
-  //               "    else\r\n"+
-  //               "    {\r\n"+
-  //               "        color *= ToonBrightnessLevels[4];\r\n"+
-  //               "    }\r\n"+
-  //               "    \r\n"+
-  //               "    gl_FragColor = vec4(color, 1.);\r\n"+
-  //               "}\r\n";
-
 		var assetsManager = new BABYLON.AssetsManager(scene)
 		assetsManager.useDefaultLoadingScreen = false
-		var meshTask = assetsManager.addMeshTask("skulltask", "", "./assets/", "bugsports.babylon")
-		meshTask.onSuccess = function (task) {
-			// var shaderMaterial = new BABYLON.ShaderMaterial("shader", scene, {
-		 //        vertex: "custom",
-		 //        fragment: "custom",
-			//     },
-		 //        {
-			// 		attributes: ["position", "normal", "uv"],
-			// 		uniforms: ["world", "worldView", "worldViewProjection", "view", "projection"]
-	  //      		})
-	        // var mainTexture = new BABYLON.Texture("./assets/3dd.jpg", scene)
-	        // shaderMaterial.setTexture("textureSampler", mainTexture)
+		assetsManager.addMeshTask("tree", "", "./assets/", "models/tree1.babylon")
+		assetsManager.addMeshTask("grass", "", "./assets/", "models/grass.babylon")
+		assetsManager.addMeshTask("bugsports", "", "./assets/", "models/bugsports.babylon")
+		assetsManager.addMeshTask("bugsports2", "", "./assets/", "models/bugsports2.babylon")
+		assetsManager.addMeshTask("chamonix", "", "./assets/", "models/chamonix.babylon")
 
-			var materialSphere1 = new BABYLON.StandardMaterial("texture1", scene)
-			materialSphere1.specularPower = 99
-			// materialSphere1.diffuseColor = new BABYLON.Color3(1.0, 0.2, 0.7)
 
-			// console.log(BABYLON.CellMaterial)
-
-			// var knot = BABYLON.Mesh.CreateTorusKnot("knot", 2, 0.5, 128, 64, 2, 3, scene)
-			var cell = new BABYLON.CellMaterial("cell", scene)
+		// var grassTask = assetsManager.addMeshTask("grass", "", "./assets/", "models/grass.babylon")
+		// grassTask.onSuccess = function (task) {
+		// 	var cell = new BABYLON.CellMaterial("cell", scene)
 			
-			cell.diffuseTexture = new BABYLON.Texture("./assets/3dd.jpg", scene)
-			cell.computeHighLevel = true
-			cell.diffuseTexture.uScale = cell.diffuseTexture.vScale = 3;
+		// 	cell.diffuseTexture = new BABYLON.Texture("./assets/3dd.jpg", scene)
+		// 	cell.computeHighLevel = true
+		// 	cell.diffuseTexture.uScale = cell.diffuseTexture.vScale = 3;
 
-			// cell.pointsCloud = true
-			// cell.pointSize = 50
-			// cell.wireframe = true
+		// 	// cell.pointsCloud = true
+		// 	// cell.pointSize = 50
+		// 	// cell.wireframe = true
 
-			//set root object position`
-			task.loadedMeshes[0].position = BABYLON.Vector3.Zero();
+		// 	//set root object position`
+		// 	task.loadedMeshes[0].position = BABYLON.Vector3.Zero()
 
-			for(let i = 0; i < task.loadedMeshes.length; i ++){
-				// task.loadedMeshes[i].useVertexColors = true
-				task.loadedMeshes[i].material = cell
+		// 	console.log(task)
 
-				// task.loadedMeshes[i].outlineWidth = 0.15
-				// task.loadedMeshes[i].outlineColor = new BABYLON.Color4(0, 0, 0, 1)
-				// task.loadedMeshes[i].renderOutline = true
+		// 	for(let i = 0; i < task.loadedMeshes.length; i ++){
+		// 		// task.loadedMeshes[i].useVertexColors = true
+		// 		task.loadedMeshes[i].material = cell
+
+		// 		// task.loadedMeshes[i].outlineWidth = 0.15
+		// 		// task.loadedMeshes[i].outlineColor = new BABYLON.Color4(0, 0, 0, 1)
+		// 		// task.loadedMeshes[i].renderOutline = true
 				
-				// task.loadedMeshes[i].enableEdgesRendering()
-				// task.loadedMeshes[i].edgesWidth = 30.0;
-				// task.loadedMeshes[i].edgesColor = new BABYLON.Color4(0, 0, 0, 1)
+		// 		// task.loadedMeshes[i].enableEdgesRendering()
+		// 		// task.loadedMeshes[i].edgesWidth = 30.0;
+		// 		// task.loadedMeshes[i].edgesColor = new BABYLON.Color4(0, 0, 0, 1)
 
-				task.loadedMeshes[i].convertToFlatShadedMesh()
-			}
+		// 		task.loadedMeshes[i].convertToFlatShadedMesh()
+		// 	}
 
-			let d = 10
-			let spacing = 150
-			for(let i = 0; i < d; i++){
-    			for(let j = 0; j < d; j++){
-    				var newInstance = task.loadedMeshes[0].createInstance("index: " + i + "" + j)
-    				newInstance.position.x =+ i * spacing
-    				newInstance.position.z =+ j * spacing
+		// 	let d = 100
+		// 	let spacing = 15
+		// 	for(let i = 0; i < d; i++){
+  //   			for(let j = 0; j < d; j++){
+  //   				var newInstance = task.loadedMeshes[0].createInstance("index: " + i + "" + j)
+  //   				newInstance.position.x =+ i * spacing
+  //   				newInstance.position.z =+ j * spacing
 
-    	// 			newInstance.enableEdgesRendering()
-				 //   	newInstance.edgesWidth = 30.0;
-					// newInstance.edgesColor = new BABYLON.Color4(0, 0, 0, 1)
-    			}
-			}
-		}
+  //   	// 			newInstance.enableEdgesRendering()
+		// 		 //   	newInstance.edgesWidth = 30.0;
+		// 			// newInstance.edgesColor = new BABYLON.Color4(0, 0, 0, 1)
+  //   			}
+		// 	}
+		// }
 
-		var textureTask = assetsManager.addTextureTask("imagetask", "./assets/dd.jpg");
+		// var textureTask = assetsManager.addTextureTask("imagetask", "./assets/dd.jpg");
 		assetsManager.load()
 
 		var test
